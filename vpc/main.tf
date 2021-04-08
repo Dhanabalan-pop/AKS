f9e4600634c6e71d380ef4f5a951fd88492e068e
Dhanabalan R <dhanabalan.r@visionetsystems.com>
2021-04-08 21:20:10 +0530

    Added new public subnet

--- vpc/main.tf
+++ vpc/main.tf
@@ -53,7 +53,7 @@ resource "aws_eip" "nat_eip" {
 /* NAT */
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet.id
+  subnet_id     = aws_subnet.public_subnet.id[0]
   depends_on    = [aws_internet_gateway.ig]
   tags = {
     Name        = var.nat_name
