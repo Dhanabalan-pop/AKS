5ecf6596ee183208b976cf8e05cf2937b0cb43f5
wahaj123 <wahajakm1234@gmail.com>
2021-04-12 11:14:34 +0500

    changes to the main folder

--- provider.tf
+++ provider.tf
@@ -1,5 +1,12 @@
 provider "aws" {
-  region     = "us-west-1"
-  access_key = "AKIA5ZKPGVEJ63H6SJMZ"
-  secret_key = "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"
+  region = "us-east-1"
+  # profile = var.profile
+}
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.22.0"
+    }
+  }
 }
