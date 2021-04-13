74cc16e586090fd4be0d69daddb14db096e80625
Dhanabalan R <dhanabalan.r@visionetsystems.com>
2021-04-13 17:16:26 +0530

    Jenkinsfile|2|kubernetes Agent-default|Reverted

---
+++ backend.tf
@@ -0,0 +1,11 @@
+/*terraform {
+required_version = "~> 0.13"
+backend "s3" {
+bucket               = ""
+region               = "us-west-1"
+key                  = "backend.tfstate"
+workspace_key_prefix = "main"
+dynamodb_table       = ""
+profile              = ""
+}
+}*/
