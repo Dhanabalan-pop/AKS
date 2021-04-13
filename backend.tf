ebfbbc5d415ab4c96ecb32af3befe95f6c6bdefc
Dhanabalan R <dhanabalan.r@visionetsystems.com>
2021-04-13 19:05:04 +0530

    backend.tf|all lines|Added

--- backend.tf
+++ backend.tf
@@ -1,11 +1,10 @@
-/*terraform {
+terraform {
 required_version = "~> 0.13"
 backend "s3" {
-bucket               = ""
+bucket               = "awsbucket-terraformstate-uswest"
 region               = "us-west-1"
 key                  = "backend.tfstate"
 workspace_key_prefix = "main"
-dynamodb_table       = ""
-profile              = ""
+dynamodb_table       = "terraform-state-dynamodb"
 }
-}*/
+}
