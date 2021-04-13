80117682e703ad148cb58a157d7f9412c3a8ca7a
Dhanabalan R <dhanabalan.r@visionetsystems.com>
2021-04-13 10:47:07 +0530

    helm.sh|6|echo PATH variable /usr/local/bin|Added

--- scripts/helm.sh
+++ scripts/helm.sh
@@ -3,6 +3,7 @@
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+echo 'export PATH=$PATH:usr/local/bin' >> ~/.bashrc
 #Install promtail
 helm repo add grafana https://grafana.github.io/helm-charts --force-update
 kubectl create namespace cms-container-monitoring
