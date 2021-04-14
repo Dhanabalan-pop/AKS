01be46a3a4b67825746ab29ada116076a885d511
Dhanabalan-pop <55197096+Dhanabalan-pop@users.noreply.github.com>
2021-04-14 15:30:25 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-# output "public_key" {
-#   value = module.iam-node.public_key
-# }
-# output "private_key" {
-#   value = module.iam-node.private_key
-# }
-=======
 output "public_key" {
   value = module.iam-node.public_key
 }
@@ -16,4 +8,3 @@ output "private_key" {
 output EKSclustername{
     value=module.eks.EKSclustername
 }
->>>>>>> 37b268dde1b050b1c533d558eb6b78144cc26fd3
