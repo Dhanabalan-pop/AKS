95f787df3f8d3766bf4add3c0e7f77480e127e8a
Dhanabalan R <dhanabalan.r@visionetsystems.com>
2021-04-15 19:13:51 +0530

    helm.sh|12,15|Updated promtail and promethus values file location|Updated

--- scripts/helm.sh
+++ scripts/helm.sh
@@ -9,7 +9,7 @@ COMMENTS
 #Install promtail
 helm repo add grafana https://grafana.github.io/helm-charts --force-update
 kubectl create namespace cms-container-monitoring
-helm install promtail --namespace cms-container-monitoring grafana/promtail -f ../config/helm/promtailvalues.yaml || helm upgrade promtail --namespace cms-container-monitoring grafana/promtail -f ../config/helm/promtailvalues.yaml
+helm install promtail --namespace cms-container-monitoring grafana/promtail -f ./config/helm/promtailvalues.yaml || helm upgrade promtail --namespace cms-container-monitoring grafana/promtail -f ./config/helm/promtailvalues.yaml
 #Install Prometheus kube state metrics and Node exporter
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts --force-update
-helm install prometheus -f ../config/helm/prometheusvalues.yaml prometheus-community/prometheus --namespace cms-container-monitoring || helm upgrade prometheus -f ../config/helm/prometheusvalues.yaml prometheus-community/prometheus --namespace cms-container-monitoring
+helm install prometheus -f ./config/helm/prometheusvalues.yaml prometheus-community/prometheus --namespace cms-container-monitoring || helm upgrade prometheus -f ./config/helm/prometheusvalues.yaml prometheus-community/prometheus --namespace cms-container-monitoring
