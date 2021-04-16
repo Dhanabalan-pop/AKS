1df97534bb2a81db987a93efc504986ce48f86c6
Dhanabalan R <dhanabalan.r@visionetsystems.com>
2021-04-16 20:06:32 +0530

    Added roles for OPENID

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -6,6 +6,38 @@ resource "aws_eks_cluster" "aws_eks" {
     subnet_ids = var.private_subnets
   }
 }
+data "tls_certificate" "example" {
+  url = aws_eks_cluster.aws_eks.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "example" {
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = [data.tls_certificate.example.certificates[0].sha1_fingerprint]
+  url             = aws_eks_cluster.aws_eks.identity[0].oidc[0].issuer
+}
+
+data "aws_iam_policy_document" "example_assume_role_policy" {
+  statement {
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    effect  = "Allow"
+
+    condition {
+      test     = "StringEquals"
+      variable = "${replace(aws_iam_openid_connect_provider.example.url, "https://", "")}:sub"
+      values   = ["system:serviceaccount:kube-system:aws-node"]
+    }
+
+    principals {
+      identifiers = [aws_iam_openid_connect_provider.example.arn]
+      type        = "Federated"
+    }
+  }
+}
+
+resource "aws_iam_role" "example" {
+  assume_role_policy = data.aws_iam_policy_document.example_assume_role_policy.json
+  name               = "example"
+}
 resource "aws_eks_node_group" "node" {
   cluster_name    = aws_eks_cluster.aws_eks.name
   node_group_name = "${var.eksnode_name}-${terraform.workspace}"
