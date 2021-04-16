6b0058219752fa39bcacf6c1d7395c65467df143
Dhanabalan R <dhanabalan.r@visionetsystems.com>
2021-04-16 16:18:29 +0530

    kube-dashboard.sh

--- scripts/kube-dashboard.sh
+++ scripts/kube-dashboard.sh
@@ -7,3 +7,5 @@ kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.0.5/a
 kubectl apply -f ./config/kubernetes/eks-admin-service-account.yaml
 #Fetching the eks-admin Secret
 kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep eks-admin | awk '{print $1}')
+#Attaching Load balancer for Kubernetes Dashboard
+kubectl patch svc/kubernetes-dashboard -n kubernetes-dashboard -p '{"spec":{"type": "loadbalancer"}}'
