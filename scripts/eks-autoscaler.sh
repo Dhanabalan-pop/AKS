5720adc8b4b4ea03bd18d5e9bc51a6428835eed6
Dhanabalan R <dhanabalan.r@visionetsystems.com>
2021-04-20 00:14:03 +0530

    Try1

--- scripts/eks-autoscaler.sh
+++ scripts/eks-autoscaler.sh
@@ -18,6 +18,9 @@ esac
 kubectl annotate serviceaccount cluster-autoscaler \
   -n kube-system \
   eks.amazonaws.com/role-arn=$1
+kubectl patch deployment cluster-autoscaler \
+  -n kube-system \
+  -p '{"spec":{"template":{"metadata":{"annotations":{"cluster-autoscaler.kubernetes.io/safe-to-evict": "false"}}}}}'
 <<'COMMENTS'
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/autoscaler/master/cluster-autoscaler/cloudprovider/aws/examples/cluster-autoscaler-autodiscover.yaml
 kubectl annotate serviceaccount cluster-autoscaler \
